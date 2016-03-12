c99d85697ecf597f7afaae8e50fd9942696c84e6
SIMRANPREET <simranpreetsaini@yahoo.com>
2016-03-11 21:46:23 -0700

    Update ClassFile.java

--- GPA-APP/ClassFile.java
+++ GPA-APP/ClassFile.java
@@ -1,16 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class ClassFile {
-	private void addClassName(String className){
-	try{
+	public void addClassName(String className) throws IOException{
 	File file = new File("High School Career Classes");
-	PrintWriter pw = new PrintWriter(file);
-	pw.println(className);
-	pw.close();
-	} catch(FileNotFoundException e){
-		System.out.println("FileNotFoundException " + e.getMessage());
-}
+	FileWriter fw = new FileWriter(file,true);
+	fw.write("\n" + className);
+	fw.close();
 }
 }
