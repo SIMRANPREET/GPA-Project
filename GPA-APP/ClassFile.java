783cced3f5aead67465a3e62d569a49eb2ac6156
SIMRANPREET <simranpreetsaini@yahoo.com>
2016-03-23 21:07:41 -0600

    Update ClassFile.java

--- GPA-APP/ClassFile.java
+++ GPA-APP/ClassFile.java
@@ -3,7 +3,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/* 
+/*
  * A class that creates a file and then writes to it only.
  * Does not read file.
  */
@@ -12,14 +12,16 @@ public class ClassFile {
 	 * This method gets the text that is entered into the
 	 * textbox (above the save button on the GUI) and writes 
 	 * it to the file. 
+	 * @param className
+	 * @throws IOException
 	 */
 	public void addClassName(String className) throws IOException{
-	File file = new File("High School Career Classes.txt");
-	BufferedWriter bw = new BufferedWriter(new FileWriter(file,true));
-	if(className != null && !className.isEmpty()){
-	bw.write(className);
-	bw.newLine();
+		File file = new File("High School Career Classes.txt");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(file,true));
+		if(className != null && !className.isEmpty()){
+			bw.write(className);
+			bw.newLine();
+		}
+		bw.close();
 	}
-	bw.close();
-}
 }
