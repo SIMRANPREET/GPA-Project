a7d8e8489890ff608e799bdae0fac958712e46d1
SIMRANPREET <simranpreetsaini@yahoo.com>
2016-03-23 22:13:36 -0600

    Update GPATRACKER.java

--- GPA-APP/GPATRACKER.java
+++ GPA-APP/GPATRACKER.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
-
 import javax.swing.JFrame;
-
 
 /*
  * The class with the entry point. Creates the main GUI frame
@@ -9,19 +7,18 @@ import javax.swing.JFrame;
  * the file. (the file that saves all the course names using
  * the ClassFile class)
  */
-
 public class GPATRACKER{
 
 	public static void main(String[] args) throws IOException {
 		GPAFRAME frame = new GPAFRAME();
 		ShowClassFrame scf = new ShowClassFrame();
-	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	      scf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	      frame.setVisible(true);
-	      scf.setVisible(true);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    scf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true);
+	    scf.setVisible(true);
 	}
-
 }
+
 
 
 
