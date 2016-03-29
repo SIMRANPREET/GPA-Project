5e9bbaf9554f117949343ba220a71efe7d568e4c
SIMRANPREET <simranpreetsaini@yahoo.com>
2016-03-29 17:46:28 -0600

    Update GPATRACKER.java

--- GPA-APP/GPATRACKER.java
+++ GPA-APP/GPATRACKER.java
@@ -11,11 +11,11 @@ public class GPATRACKER{
 
 	public static void main(String[] args) throws IOException {
 		GPAFRAME frame = new GPAFRAME();
-		ShowClassFrame scf = new ShowClassFrame();
+
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    scf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	    frame.setVisible(true);
-	    scf.setVisible(true);
+
 	}
 }
 
